7b45a90d691c6ba806e670d4e08d23a81a63bcab
RudeGalaxy1010 <Karev007@bk.ru>
2022-07-11 18:44:22 +0300

    Player movement rework | look rotation

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,32 +1,37 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private Joystick _joystick;
 
-    private Rigidbody _rigidbody;
+    private CharacterController _controller;
 
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        _controller = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
         Move();
+        Look();
     }
 
     private void Move()
     {
         if (_joystick.Direction != Vector2.zero)
         {
-            _rigidbody.velocity = new Vector3(_joystick.Direction.x, 0, _joystick.Direction.y) * _speed;
+            _controller.Move(new Vector3(_joystick.Direction.x, 0, _joystick.Direction.y) * _speed * Time.deltaTime);
         }
-        else
+    }
+
+    private void Look()
+    {
+        if (_joystick.Direction != Vector2.zero)
         {
-            _rigidbody.velocity = Vector3.zero;
+            transform.rotation = Quaternion.LookRotation(new Vector3(_joystick.Direction.x, 0, _joystick.Direction.y) * _speed * Time.deltaTime);
         }
     }
 }
