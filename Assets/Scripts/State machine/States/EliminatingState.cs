47fa6f719ee1f5313d98302d30b74598cfe03a53
RudeGalaxy1010 <Karev007@bk.ru>
2022-07-11 14:42:37 +0300

    Progress bars

--- Assets/Scripts/State machine/States/EliminatingState.cs
+++ Assets/Scripts/State machine/States/EliminatingState.cs
@@ -1,24 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EliminatingState : State
 {
     private float _timer;
+    [SerializeField] private Slider _progressBar;
 
     private void OnEnable()
     {
         _timer = 0;
+        _progressBar.maxValue = Target.EliminatingTime;
+        _progressBar.gameObject.SetActive(true);
     }
 
     private void OnDisable()
     {
-        
+        _progressBar.gameObject.SetActive(false);
     }
 
     private void Update()
-    {   
+    {
         _timer += Time.deltaTime;
+        _progressBar.value = _timer;
 
         if (_timer >= Target.EliminatingTime)
         {
--- Assets/Scripts/State machine/States/IncidentState.cs
+++ Assets/Scripts/State machine/States/IncidentState.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IncidentState : State
 {
     [SerializeField] private Incident[] _incidents;
+    [SerializeField] private Slider _progressBar;
     private float _timer;
 
     private void OnEnable()
@@ -12,16 +14,20 @@ public class IncidentState : State
             _timer = 0;
             Target.StartIncident(_incidents[Random.Range(0, _incidents.Length)]);
         }
+
+        _progressBar.maxValue = Target.MaxIncidentTime;
+        _progressBar.gameObject.SetActive(true);
     }
 
     private void OnDisable()
     {
-
+        _progressBar.gameObject.SetActive(false);
     }
 
     private void Update()
     {
         _timer += Time.deltaTime;
+        _progressBar.value = _timer;
 
         if (_timer >= Target.MaxIncidentTime)
         {
